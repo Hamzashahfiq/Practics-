019cc0926af87ad5555af2305a2172bac12dc3e5
Muhammad Hamza <hamzashafique535@gmail.com>
2022-08-08 23:02:33 +0500

    set login page

--- serverAuth/authLoginReactJS/auth-node-logic/src/pages/login/Login.js
+++ serverAuth/authLoginReactJS/auth-node-logic/src/pages/login/Login.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import NavigationTab from '../../commonComponents/NavigationTab'
+import { styles } from './LoginStyle'
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 
 export default function Login() {
   return (
-    <div style={{padding:'30px'}}>
-        <NavigationTab />
-        <h2>Login</h2>
-        <span>User Name:</span>
-        <input type='text' />
-        <span>Password:</span>
-        <input type='text' />
-          <button>Login</button>
+    <div style={styles.mainDiv}>
+      <div style={styles.innerDiv}>
+        <Typography style={styles.innerDivText} variant="h4" gutterBottom component="div">
+          Login
+        </Typography>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </Box>
+
+
+      </div>
     </div>
   )
 }
---
+++ serverAuth/authLoginReactJS/auth-node-logic/src/pages/login/LoginStyle.js
@@ -0,0 +1,19 @@
+export const styles = {
+    mainDiv:{
+        backgroundColor: '#f5ebe0',
+        height: '100vh',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        minWidth:'fit-content'
+    },
+    innerDiv:{
+        margin: '20px',
+        backgroundColor: 'white',
+        padding: '10px',
+        height : '60vh'
+    },
+    innerDivText:{
+        fontWeight:'bold'
+    }
+}
