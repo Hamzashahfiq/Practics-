4d46263c069a4bde13e327230c19eacaef11ab5e
Muhammad Hamza <hamzashafique535@gmail.com>
2022-08-27 11:41:26 +0500

    sst package.json for heruko

--- serverAuth/server.js
+++ serverAuth/server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser')
 var cors = require('cors')
 require('dotenv').config()
 
-const port = 5000
+const port = process.env.PORT || 5000
 const app = express();
 
 const AuthRouter  = require('./src/components/auth/AuthRoutes')
